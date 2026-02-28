79ca7e2cf5d82eeaf19a3f99e5eb900ccb5004fc
Robert Jordan <robertmerrilljordan@gmail.com>
2026-02-28 05:44:32 -0600

    progress

---
+++ Source/My.Talli.Web/ViewModels/Pages/LoginViewModel.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components;
+
+namespace My.Talli.Web.ViewModels.Pages;
+
+public class LoginViewModel : ComponentBase
+{
+    protected bool ShowComingSoon { get; set; }
+
+    protected void HandleGoogleSignIn()
+    {
+        ShowComingSoon = true;
+        // TODO: Wire up Google OAuth authentication
+    }
+
+    protected void HandleAppleSignIn()
+    {
+        ShowComingSoon = true;
+        // TODO: Wire up Apple Sign-In authentication
+    }
+}
