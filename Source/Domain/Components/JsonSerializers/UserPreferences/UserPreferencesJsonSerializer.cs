a6e4ae2aa813bc758316d02c608e49ca7da6d450
Robert Jordan <robertmerrilljordan@gmail.com>
2026-03-14 12:51:13 -0500

    progress

---
+++ Source/Domain/Components/JsonSerializers/UserPreferences/UserPreferencesJsonSerializer.cs
@@ -0,0 +1,37 @@
+namespace My.Talli.Domain.Components.JsonSerializers.UserPreferences;
+
+using System.Text.Json;
+using My.Talli.Domain.Framework;
+using Models = My.Talli.Domain.Models;
+
+/// <summary>Component</summary>
+public class UserPreferencesJsonSerializer
+{
+	#region <Variables>
+
+	private static readonly JsonSerializerOptions Options = new()
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false
+	};
+
+	#endregion
+
+	#region <Methods>
+
+	public Models.UserPreferences Deserialize(string json)
+	{
+		Assert.IsNullOrWhitespace(json, nameof(json));
+
+		return JsonSerializer.Deserialize<Models.UserPreferences>(json, Options) ?? new Models.UserPreferences();
+	}
+
+	public string Serialize(Models.UserPreferences preferences)
+	{
+		Assert.IsNotNull(preferences, nameof(preferences));
+
+		return JsonSerializer.Serialize(preferences, Options);
+	}
+
+	#endregion
+}
---
+++ Source/Domain/Models/EmailPreferences.cs
@@ -0,0 +1,15 @@
+namespace My.Talli.Domain.Models;
+
+/// <summary>Model</summary>
+public class EmailPreferences
+{
+	#region <Properties>
+
+	public bool SubscriptionConfirmationEmail { get; set; } = true;
+
+	public bool UnsubscribeAll { get; set; }
+
+	public bool WeeklySummaryEmail { get; set; } = true;
+
+	#endregion
+}
