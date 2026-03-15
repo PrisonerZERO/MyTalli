09074713d7697e923f5982c997a0c0578ea49ef7
Robert Jordan <robertmerrilljordan@gmail.com>
2026-03-15 04:45:14 -0500

    progress

---
+++ Source/Domain/Mappers/MappingProfile.cs
@@ -0,0 +1,15 @@
+namespace My.Talli.Domain.Mappers;
+
+using AutoMapper;
+
+/// <summary>Profile</summary>
+public class MappingProfile : Profile
+{
+    #region <Constructors>
+
+    public MappingProfile()
+    {
+    }
+
+    #endregion
+}
