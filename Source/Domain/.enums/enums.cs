3c8ebd7f14ef6e1ae974853dda869e105c2cd2eb
Robert Jordan <robertmerrilljordan@gmail.com>
2026-04-25 06:27:12 -0500

    progress

--- Source/Domain/.enums/enums.cs
+++ Source/Domain/.enums/enums.cs
@@ -1,1 +1,13 @@
-﻿namespace My.Talli.Domain.enums;
+﻿
+namespace My.Talli.Domain.enums;
+
+using Domain.attributes;
+
+public enum EtsyLedgerTypes
+{ 
+    [StringValue("Foo")]
+    Foo,
+
+    [StringValue("Bar")]
+    Bar,
+}
---
+++ Source/Domain/.extensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace My.Talli.Domain.extensions;
+
+using Domain.attributes;
+using System;
+
+/// <summary>Extensions</summary>
+public static class EnumExtensions
+{
+    #region <Methods>
+
+    public static string ToStringValue(this Enum value)
+    {
+        var type = value.GetType();
+        var fieldInfo = type.GetField(value.ToString());
+        var result = fieldInfo.GetCustomAttributes(typeof(StringValueAttribute), false) as StringValueAttribute[];
+
+        return result != null && result.Length > 0 ? result[0].StringValue : string.Empty;
+    }
+
+    #endregion
+}
